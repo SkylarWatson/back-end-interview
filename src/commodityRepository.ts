d0cfc7e61718b751e38e5b1f0bfa347bb6eac289
Skylar <skylarrwatson@gmail.com>
2023-03-05 17:20:42 -0600

    Added commodity repository to retrieve commodity information

---
+++ src/commodityRepository.ts
@@ -0,0 +1,21 @@
+import * as fs from "fs";
+import * as path from "path";
+import { parse } from 'csv-parse/sync';
+
+class CommodityRepository {
+   pathToCsv: string = './resources/Projection2021.csv';
+
+	findAll(): any {
+		const csvFilePath = path.resolve(__dirname, this.pathToCsv);
+		const fileContent = fs.readFileSync(csvFilePath, { encoding: 'utf-8' });
+
+		return parse(fileContent, {
+			columns: true,
+			delimiter: ',',
+			from_line: 1,
+			bom: true
+		});
+	}
+}
+
+export { CommodityRepository }
