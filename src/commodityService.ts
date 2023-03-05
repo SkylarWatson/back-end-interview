d40a51b0102f9de07825a483f420dd270da7dbdb
Skylar <skylarrwatson@gmail.com>
2023-03-05 17:58:23 -0600

    Introduced commodity service to return a histogram by field/column name

---
+++ src/commodityService.ts
@@ -0,0 +1,29 @@
+import { CommodityRepository } from './commodityRepository';
+
+class CommodityService {
+	repository: CommodityRepository;
+
+    constructor(repository?: CommodityRepository) {
+  		this.repository = repository ? repository : new CommodityRepository();
+    }
+
+	findAll() : Map<string,string>[] {
+		return this.repository.findAll();
+	}
+	
+	histogram(fieldName:string) : Map<string,number> {
+		const result = new Map<string, number>();
+
+		this.findAll().forEach((record) => {
+			let fieldValue = record[fieldName];
+			let currentCount = result.get(fieldValue);
+			currentCount = currentCount ?? 0;
+			result.set(fieldValue, currentCount + 1);
+		});
+
+		return result;
+	}
+	
+}
+
+export { CommodityService }
---
+++ tests/commodityService.spec.ts
@@ -0,0 +1,28 @@
+import { CommodityRepository } from '../src/commodityRepository';
+import { CommodityService } from '../src/commodityService';
+import { mock, instance, when } from 'ts-mockito';
+
+
+describe('Service Test', () => {	
+  const repository: CommodityRepository = mock(CommodityRepository);
+  const result = [
+	  {'Commodity': 'Foo'},
+	  {'Commodity': 'Baz', 'OtherField': 'Foo'},
+	  {'Commodity': 'Foo', 'OtherField': 'Foo'}
+  ];
+
+
+  test('findAll should return commodities from the repository', () => {	  
+	  when(repository.findAll()).thenReturn(result);
+	  expect(new CommodityService(instance(repository)).findAll()).toBe(result);
+  });
+  
+  test('create histogram from commodities', () => {
+	  when(repository.findAll()).thenReturn(result);
+
+	  const commodities = new CommodityService(instance(repository)).histogram('Commodity');
+
+	  expect(commodities.get('Foo')).toBe(2);
+	  expect(commodities.get('Baz')).toBe(1);
+  });
+});
